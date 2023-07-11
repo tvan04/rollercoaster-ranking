93bf4cb9c6456d3d6559035423f09d7f9150679a
tvan04 <vantristan04@gmail.com>
2023-07-11 16:22:33 -0400

    move files to main folder

--- src/components/Coaster.jsx
+++ src/components/Coaster.jsx
@@ -1,11 +1,16 @@
 function Coaster(props) {
+    function deleteCoaster() {
+        console.log(props.name)
+
+    }
   return (
     <div className="card">
         <h2>{props.name}</h2>
         <div className = "actions">
+            <button className="btn">About</button>
             <button className="btn">+</button>
             <button className="btn">-</button>
-            <button className="btn">🗑️</button>
+            <button className="btn" onClick={deleteCoaster}>🗑️</button>
         </div>
     </div>
   );
